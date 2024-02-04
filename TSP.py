bb0398dd8e66c70df1e05525bc5e05ad9a5d0f60
Mustafasohail7 <mustufasohail7@gmail.com>
2024-02-04 19:42:28 +0500

    removed Chromosome separate class

--- TSP.py
+++ TSP.py
@@ -1,55 +1,8 @@
 import random 
+import math
+import numpy as np
 from problem import Problem
 
-class Chromosome:
-    def __init__(self,chromosome):
-        self.chromosome = chromosome
-        self.fitness = self.calculate_fitness()
-
-    def get_chromosome(self):
-        return self.chromosome
-
-    def calculate_fitness(self):
-        total_distance = 0.0
-        num_cities = len(self.chromosome)
-
-        for i in range(num_cities - 1):
-            # Calculate Euclidean distance between consecutive cities
-            city1 = self.chromosome[i]
-            city2 = self.chromosome[i + 1]
-            distance = math.sqrt((city2[1] - city1[1])**2 + (city2[2] - city1[2])**2)
-            total_distance += distance
-
-        # Add distance from the last city back to the starting city
-        total_distance += math.sqrt((self.chromosome[-1][1] - self.chromosome[0][1])**2 + 
-                                    (self.chromosome[-1][2] - self.chromosome[0][2])**2)
-
-        # Fitness is the inverse of the total distance
-        fitness = 1 / total_distance
-
-        return fitness
-
-    def crossover(self, other):
-        # Perform crossover to create a new chromosome from two parents
-        crossover_point = np.random.randint(1, len(self.chromosome) - 1)
-
-        # Create a new chromosome by combining parts of both parents
-        new_chromosome = np.concatenate((self.chromosome[:crossover_point], other.chromosome[crossover_point:]))
-
-        return Chromosome(new_chromosome)
-
-    def mutate(self):
-        # Perform mutation by swapping two cities in the chromosome
-        mutation_point1, mutation_point2 = np.random.choice(len(self.chromosome), 2, replace=False)
-
-        # Create a new chromosome with the cities swapped
-        new_chromosome = np.copy(self.chromosome)
-        new_chromosome[mutation_point1], new_chromosome[mutation_point2] = (
-            new_chromosome[mutation_point2],
-            new_chromosome[mutation_point1]
-        )
-
-        return Chromosome(new_chromosome)
 
 class TSP(Problem):
 
@@ -61,22 +14,58 @@ class TSP(Problem):
         self.population = []
         for i in range(self.population_size):
             self.population.append(self.random_chromosome())
+        print(self.population[0])
 
 
-    def fitness(self,chromosome):
-        pass
+    def calculate_fitness(self,chromosome):
+        total_distance = 0.0
+        num_cities = len(chromosome)
 
-    def crossover(self,chromosome1,chromosome2):
-        pass
+        for i in range(num_cities - 1):
+            # Calculate Euclidean distance between consecutive cities
+            city1 = self.data[chromosome[i] - 1]
+            city2 = self.data[chromosome[i + 1] - 1]
+            distance = math.sqrt((city2[0] - city1[0])**2 + (city2[1] - city1[1])**2)
+            total_distance += distance
 
-    def mutation(self,chromosome):
-        pass
+        # Add distance from the last city back to the starting city
+        total_distance += math.sqrt((self.data[-1][0] - self.data[0][0])**2 + 
+                                    (self.data[-1][1] - self.data[0][1])**2)
+
+        # Fitness is the inverse of the total distance
+        fitness = 1 / total_distance
+
+        return fitness
+    
+    def crossover(self,chromosome, other):
+        # Perform crossover to create a new chromosome from two parents
+        crossover_point = np.random.randint(1, len(chromosome) - 1)
+
+        # Create a new chromosome by combining parts of both parents
+        new_chromosome = np.concatenate((chromosome[:crossover_point], other.chromosome[crossover_point:]))
+
+        return new_chromosome
+
+    def mutate(self,chromosome):
+        # Perform mutation by swapping two cities in the chromosome
+        mutation_point1, mutation_point2 = np.random.choice(len(chromosome), 2, replace=False)
+
+        # Create a new chromosome with the cities swapped
+        new_chromosome = np.copy(chromosome)
+        new_chromosome[mutation_point1], new_chromosome[mutation_point2] = (
+            new_chromosome[mutation_point2],
+            new_chromosome[mutation_point1]
+        )
+
+        return new_chromosome
 
     def random_chromosome(self):
         #generate random chromosome from TSP set
         solution = list(range(1,len(self.data)+1))
         random.shuffle(solution)
-        return Chromosome(solution).get_chromosome()
+        fitness = self.calculate_fitness(solution)
+        chromosome = (solution,fitness)
+        return chromosome
 
 class EA:
 
@@ -123,11 +112,12 @@ class ReadFile:
     #formats city data
     def parse_city_data(self,city_data):
         city = city_data.split()
-        city = (int(city[0]),float(city[1]),float(city[2]))
+        city = (float(city[1]),float(city[2]))
         return city
 
 def main():
     data = ReadFile("qa194.tsp").read()
+    # print(data[0])
     EA(30, 10, 50, 0.5, 10,data)
     # print(data)
 
