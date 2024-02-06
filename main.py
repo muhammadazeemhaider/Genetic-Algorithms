29f6e68a9d02269f9ac2a6cdf5e4c17df8497fba
Mustafasohail7 <mustufasohail7@gmail.com>
2024-02-06 14:25:32 +0500

    code now runs through arguments passed on console

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import sys
 from EA import EA
 
 def main():
-    filename = "qa194.tsp"
+    filename = sys.argv[2]
     pop_size = 30
     offspring_size = 10
     generations_no = 50
     mutation_rate = 0.5
     iterations = 10
-    problem = "TSP"
-    parent_selection = "tournament_selection_23"
-    survivor_selection = "truncation"
+    problem = sys.argv[1]
+    parent_selection = sys.argv[3]
+    survivor_selection = sys.argv[4]
     EA(pop_size, offspring_size, generations_no, mutation_rate, iterations, problem, parent_selection, survivor_selection, filename).run()
 
 main()
